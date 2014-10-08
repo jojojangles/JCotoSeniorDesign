5ed03d5646f261955245d4bc61aea039a8423ecd
jojojangles <jcotosax@gmail.com>
2014-10-08 00:02:48 -0400

    Tutorial 2 complete

---
+++ ADAPT/ADAPTp1/Unity/Assets/ADAPT Core/Tutorials/Tutorial2/TutorialSteeringCoordinator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialSteeringCoordinator : ShadowCoordinator {
+	protected ShadowTransform[] buffer1 = null;
+	protected ShadowLocomotionController loco = null;
+	// Use this for initialization
+	void Start () {
+		this.buffer1 = this.NewTransformArray();
+		this.loco = this.GetComponent<ShadowLocomotionController>();
+		this.ControlledStartAll();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.UpdateCoordinates(); //moves root pos of each shadow to match display model
+		this.loco.ControlledUpdate(); //update lean controller write shadow into buff
+		this.loco.Encode(this.buffer1);
+
+		Shadow.ReadShadowData(//write shadow buffer to display model starting at hips
+		                      this.buffer1,
+		                      this.transform.GetChild(0),
+		                      this);
+	}
+}
---
+++ ADAPT/ADAPTp1/Unity/Assets/ADAPT Core/Tutorials/Tutorial2/TutorialWaypoint.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialWaypoint : MonoBehaviour {
+	public KeyCode code;
+	public UnitySteeringController controller;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(this.code)){
+			this.controller.Target = transform.position;
+		}
+	}
+}
